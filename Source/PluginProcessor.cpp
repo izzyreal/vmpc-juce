a20762693ddf0a86bba8a5604bc6dcd4c6234317
Izmar Verhage <izmaelverhage@gmail.com>
2020-03-22 16:48:10 +0100

    Use 5 stereo output channel pairs

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -37,7 +37,7 @@ VmpcAudioProcessor::VmpcAudioProcessor()
 #endif
 {
 	mpc = new mpc::Mpc();
-	mpc->init(44100.f, 1, 1);
+	mpc->init(44100.f, 1, 5);
 	mpc->getLayeredScreen().lock()->openScreen("sequencer");
 }
 
