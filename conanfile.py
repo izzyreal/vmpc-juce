c0498d55e145aaa28308ffa51702572c5cef68a9
Izmar Verhage <izmaelverhage@gmail.com>
2020-10-18 09:13:46 +0200

    Add generator and lto flags

--- conanfile.py
+++ conanfile.py
@@ -1,11 +1,11 @@
 from conans import ConanFile, CMake
 
 class Pkg(ConanFile):
-    name = "vmpc"
+    name = "vmpc-juce"
     description = "JUCE implementation of vMPC2000XL, an Akai MPC2000XL emulator"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
-    generators = "cmake"
+    generators = "cmake", "cmake_find_package"
     requires = ("juce/0.1@izmar/dev", "mpc/0.1@izmar/dev", "ctoot/0.1@izmar/dev", "moduru/0.1@izmar/dev")
     url = "https://github.com/izzyreal/vmpc-juce.git"
 	
