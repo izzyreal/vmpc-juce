67cd2ebe520168eca23ac86e5529eacdd7b2daac
Izmar Verhage <izmarverhage@lunatech.nl>
2021-01-19 12:38:12 +0100

    Do not MLOG key events

--- src/main/gui/KeyEventListener.cpp
+++ src/main/gui/KeyEventListener.cpp
@@ -3,8 +3,6 @@
 #include <controls/KeyEvent.hpp>
 #include <controls/KeyEventHandler.hpp>
 
-#include <Logger.hpp>
-
 using namespace mpc::controls;
 using namespace std;
 
@@ -19,8 +17,6 @@ bool KeyEventListener::keyPressed(const juce::KeyPress &key) {
 
 bool KeyEventListener::keyEvent(const juce::KeyEvent &keyEvent) {
     DBG("KeyEventListener::keyEvent rawKeyCode " << keyEvent.rawKeyCode << (keyEvent.keyDown ? " down" : " up"));
-    MLOG("KeyEventListener::keyEvent rawKeyCode " + std::to_string(keyEvent.rawKeyCode) + (keyEvent.keyDown ? " down" : " up"));
     keyEventHandler.lock()->handle(KeyEvent(keyEvent.rawKeyCode, keyEvent.keyDown));
-    
     return true;
 }
