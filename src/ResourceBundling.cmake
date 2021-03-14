1a98d206572e5ba78cefc9007d90ed67be80d326
Izmar Verhage <izmaelverhage@gmail.com>
2021-03-14 11:36:24 +0100

    Extract CMake bundle function

---
+++ src/ResourceBundling.cmake
@@ -0,0 +1,15 @@
+function(_vmpc_bundle_resources target_name)
+    set(_rsrc_root_path "${CMAKE_CURRENT_SOURCE_DIR}/../resources")
+
+    file(
+        GLOB_RECURSE _resource_list
+        LIST_DIRECTORIES false
+        RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
+        "${_rsrc_root_path}/*.png"
+    )
+
+    foreach(_resource IN ITEMS ${_resource_list})
+        target_sources(${target_name} PUBLIC ${_resource})
+        set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION Resources)
+    endforeach()
+endfunction()
