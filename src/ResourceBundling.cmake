d9c91af25d85ae9e6a0bb34474ccb627ed93f6c3
Izmar Verhage <izmaelverhage@gmail.com>
2021-03-15 21:40:09 +0100

    Move resources

--- src/ResourceBundling.cmake
+++ src/ResourceBundling.cmake
@@ -1,11 +1,17 @@
 function(_vmpc_bundle_resources target_name)
+    _bundle_img(${target_name})
+endfunction()
+
+function(_bundle_img target_name)
     set(_rsrc_root_path "${CMAKE_CURRENT_SOURCE_DIR}/../resources")
 
     file(
         GLOB_RECURSE _resource_list
         LIST_DIRECTORIES false
         RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
+        "${_rsrc_root_path}/*.gif"
         "${_rsrc_root_path}/*.png"
+        "${_rsrc_root_path}/*.jpg"
     )
 
     foreach(_resource IN ITEMS ${_resource_list})
