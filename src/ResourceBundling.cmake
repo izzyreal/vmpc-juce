35fd114886546d44f43f0b1675afec907c041941
Izmar Verhage <izmaelverhage@gmail.com>
2021-03-16 09:01:51 +0100

    Fix Xcode project groups

--- src/ResourceBundling.cmake
+++ src/ResourceBundling.cmake
@@ -9,15 +9,16 @@ function(_bundle_img target_name)
     file(
         GLOB_RECURSE _resource_list
         LIST_DIRECTORIES false
-        #RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
+        RELATIVE "${CMAKE_CURRENT_SOURCE_DIR}"
         "${_rsrc_root_path}/*.gif"
         "${_rsrc_root_path}/*.png"
         "${_rsrc_root_path}/*.jpg"
     )
 
     foreach(_resource IN ITEMS ${_resource_list})
+        set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/img)
+	source_group("Resources/img" FILES "${_resource}")
         target_sources(${target_name} PUBLIC ${_resource})
-        set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/img")
     endforeach()
 endfunction()
 
@@ -40,8 +41,9 @@ function(_bundle_fonts target_name)
     )
 
     foreach(_resource IN ITEMS ${_resource_list})
-        target_sources(${target_name} PUBLIC ${_resource})
         set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/fonts")
+	source_group("Resources/fonts" FILES "${_resource}")
+        target_sources(${target_name} PUBLIC ${_resource})
     endforeach()
 endfunction()
 
@@ -56,8 +58,10 @@ function(_bundle_screens target_name)
     )
 
     foreach(_resource IN ITEMS ${_resource_list})
+        set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/
+screens")
+	source_group("Resources/screens" FILES "${_resource}")
         target_sources(${target_name} PUBLIC ${_resource})
-        set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/screens")
     endforeach()
 endfunction()
 
@@ -72,8 +76,9 @@ function(_bundle_screens_bg target_name)
     )
 
     foreach(_resource IN ITEMS ${_resource_list})
-        target_sources(${target_name} PUBLIC ${_resource})
         set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/screens/bg")
+	source_group("Resources/screens/bg" FILES "${_resource}")
+        target_sources(${target_name} PUBLIC ${_resource})
     endforeach()
 endfunction()
 
@@ -88,7 +93,8 @@ function(_bundle_audio target_name)
     )
 
     foreach(_resource IN ITEMS ${_resource_list})
-        target_sources(${target_name} PUBLIC ${_resource})
         set_source_files_properties(${_resource} PROPERTIES MACOSX_PACKAGE_LOCATION "Resources/audio")
+	source_group("Resources/audio" FILES "${_resource}")
+        target_sources(${target_name} PUBLIC ${_resource})
     endforeach()
 endfunction()
