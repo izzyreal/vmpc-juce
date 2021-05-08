dcc61a6daa5050f6b7afa14a5e7612f7cf34f8be
Izmar Verhage <izmaelverhage@gmail.com>
2021-05-08 22:41:18 +0200

    VMPC-4 Fix numpad mouse support

--- src/main/gui/ButtonControl.cpp
+++ src/main/gui/ButtonControl.cpp
@@ -4,59 +4,58 @@
 using namespace juce;
 using namespace std;
 
-ButtonControl::ButtonControl(Rectangle<float> rect, std::weak_ptr<mpc::hardware::Button> button)
+ButtonControl::ButtonControl(Rectangle<int> _rect, weak_ptr<mpc::hardware::Button> _button)
+: rect (_rect), button (_button)
 {
-    this->button = button;
-    this->rect = rect;
 }
 
-std::unordered_map<std::string, Rectangle<float>*> ButtonControl::rects;
-Rectangle<float> ButtonControl::undoseq = Rectangle<float>(212, 652, mw, mh);
-Rectangle<float> ButtonControl::erase = Rectangle<float>(291, 651, mw, mh);
-Rectangle<float> ButtonControl::rec = Rectangle<float>(208, 808, bw, bh);
-Rectangle<float> ButtonControl::overdub = Rectangle<float>(288, 806, bw, bh);
-Rectangle<float> ButtonControl::stop = Rectangle<float>(367, 804, bw, bh);
-Rectangle<float> ButtonControl::play = Rectangle<float>(446, 802, bw, bh);
-Rectangle<float> ButtonControl::playstart = Rectangle<float>(525, 800, bw, bh);
-Rectangle<float> ButtonControl::mainscreen = Rectangle<float>(382, 307, mw, mh);
-Rectangle<float> ButtonControl::openwindow = Rectangle<float>(510, 308, mw, mh);
-Rectangle<float> ButtonControl::taptemponoterepeat = Rectangle<float>(247, 556, bw, bh);
-Rectangle<float> ButtonControl::prevstepevent = Rectangle<float>(212, 723, mw, mh);
-Rectangle<float> ButtonControl::nextstepevent = Rectangle<float>(292, 721, mw, mh);
-Rectangle<float> ButtonControl::gotoRect = Rectangle<float>(372, 719, mw, mh);
-Rectangle<float> ButtonControl::prevbarstart = Rectangle<float>(450, 716, mw, mh);
-Rectangle<float> ButtonControl::nextbarend = Rectangle<float>(528, 718, mw, mh);
-Rectangle<float> ButtonControl::f1 = Rectangle<float>(144, 262, smw, smh);
-Rectangle<float> ButtonControl::f2 = Rectangle<float>(225, 261, smw, smh);
-Rectangle<float> ButtonControl::f3 = Rectangle<float>(310, 263, smw, smh);
-Rectangle<float> ButtonControl::f4 = Rectangle<float>(394, 263, smw, smh);
-Rectangle<float> ButtonControl::f5 = Rectangle<float>(474, 261, smw, smh);
-Rectangle<float> ButtonControl::f6 = Rectangle<float>(554, 262, smw, smh);
-Rectangle<float> ButtonControl::notevariationafter = Rectangle<float>(88, 568, mw, mh);
-Rectangle<float> ButtonControl::rect0 = Rectangle<float>(170, 460, smw, smh);
-Rectangle<float> ButtonControl::rect1 = Rectangle<float>(91, 413, smw, smh);
-Rectangle<float> ButtonControl::rect2 = Rectangle<float>(174, 414, smw, smh);
-Rectangle<float> ButtonControl::rect3 = Rectangle<float>(253, 415, smw, smh);
-Rectangle<float> ButtonControl::rect4 = Rectangle<float>(91, 365, smw, smh);
-Rectangle<float> ButtonControl::rect5 = Rectangle<float>(173, 364, smw, smh);
-Rectangle<float> ButtonControl::rect6 = Rectangle<float>(253, 365, smw, smh);
-Rectangle<float> ButtonControl::rect7 = Rectangle<float>(88, 315, smw, smh);
-Rectangle<float> ButtonControl::rect8 = Rectangle<float>(170, 315, smw, smh);
-Rectangle<float> ButtonControl::rect9 = Rectangle<float>(251, 315, smw, smh);
-Rectangle<float> ButtonControl::shift = Rectangle<float>(90, 462, smw, smh);
-Rectangle<float> ButtonControl::enter = Rectangle<float>(253, 460, smw, smh);
-Rectangle<float> ButtonControl::banka = Rectangle<float>(942, 259, mw, mh);
-Rectangle<float> ButtonControl::bankb = Rectangle<float>(1026, 260, mw, mh);
-Rectangle<float> ButtonControl::bankc = Rectangle<float>(1110, 257, mw, mh);
-Rectangle<float> ButtonControl::bankd = Rectangle<float>(1192, 258, mw, mh);
-Rectangle<float> ButtonControl::fulllevel = Rectangle<float>(777, 178, mw, mh);
-Rectangle<float> ButtonControl::sixteenlevels = Rectangle<float>(861, 180, mw, mh);
-Rectangle<float> ButtonControl::nextseq = Rectangle<float>(778, 263, mw, mh);
-Rectangle<float> ButtonControl::trackmute = Rectangle<float>(860, 261, mw, mh);
-Rectangle<float> ButtonControl::left = Rectangle<float>(416, 581, 29, 76);
-Rectangle<float> ButtonControl::up = Rectangle<float>(445, 581, 46, 38);
-Rectangle<float> ButtonControl::down = Rectangle<float>(445, 619, 46, 38);
-Rectangle<float> ButtonControl::right = Rectangle<float>(491, 581, 29, 76);
+unordered_map<string, Rectangle<int>*> ButtonControl::rects;
+Rectangle<int> ButtonControl::undoseq = Rectangle<int>(212, 652, mw, mh);
+Rectangle<int> ButtonControl::erase = Rectangle<int>(291, 651, mw, mh);
+Rectangle<int> ButtonControl::rec = Rectangle<int>(208, 808, bw, bh);
+Rectangle<int> ButtonControl::overdub = Rectangle<int>(288, 806, bw, bh);
+Rectangle<int> ButtonControl::stop = Rectangle<int>(367, 804, bw, bh);
+Rectangle<int> ButtonControl::play = Rectangle<int>(446, 802, bw, bh);
+Rectangle<int> ButtonControl::playstart = Rectangle<int>(525, 800, bw, bh);
+Rectangle<int> ButtonControl::mainscreen = Rectangle<int>(382, 307, mw, mh);
+Rectangle<int> ButtonControl::openwindow = Rectangle<int>(510, 308, mw, mh);
+Rectangle<int> ButtonControl::taptemponoterepeat = Rectangle<int>(247, 556, bw, bh);
+Rectangle<int> ButtonControl::prevstepevent = Rectangle<int>(212, 723, mw, mh);
+Rectangle<int> ButtonControl::nextstepevent = Rectangle<int>(292, 721, mw, mh);
+Rectangle<int> ButtonControl::gotoRect = Rectangle<int>(372, 719, mw, mh);
+Rectangle<int> ButtonControl::prevbarstart = Rectangle<int>(450, 716, mw, mh);
+Rectangle<int> ButtonControl::nextbarend = Rectangle<int>(528, 718, mw, mh);
+Rectangle<int> ButtonControl::f1 = Rectangle<int>(144, 262, smw, smh);
+Rectangle<int> ButtonControl::f2 = Rectangle<int>(225, 261, smw, smh);
+Rectangle<int> ButtonControl::f3 = Rectangle<int>(310, 263, smw, smh);
+Rectangle<int> ButtonControl::f4 = Rectangle<int>(394, 263, smw, smh);
+Rectangle<int> ButtonControl::f5 = Rectangle<int>(474, 261, smw, smh);
+Rectangle<int> ButtonControl::f6 = Rectangle<int>(554, 262, smw, smh);
+Rectangle<int> ButtonControl::notevariationafter = Rectangle<int>(88, 568, mw, mh);
+Rectangle<int> ButtonControl::rect0 = Rectangle<int>(170, 460, smw, smh);
+Rectangle<int> ButtonControl::rect1 = Rectangle<int>(91, 413, smw, smh);
+Rectangle<int> ButtonControl::rect2 = Rectangle<int>(174, 414, smw, smh);
+Rectangle<int> ButtonControl::rect3 = Rectangle<int>(253, 415, smw, smh);
+Rectangle<int> ButtonControl::rect4 = Rectangle<int>(91, 365, smw, smh);
+Rectangle<int> ButtonControl::rect5 = Rectangle<int>(173, 364, smw, smh);
+Rectangle<int> ButtonControl::rect6 = Rectangle<int>(253, 365, smw, smh);
+Rectangle<int> ButtonControl::rect7 = Rectangle<int>(88, 315, smw, smh);
+Rectangle<int> ButtonControl::rect8 = Rectangle<int>(170, 315, smw, smh);
+Rectangle<int> ButtonControl::rect9 = Rectangle<int>(251, 315, smw, smh);
+Rectangle<int> ButtonControl::shift = Rectangle<int>(90, 462, smw, smh);
+Rectangle<int> ButtonControl::enter = Rectangle<int>(253, 460, smw, smh);
+Rectangle<int> ButtonControl::banka = Rectangle<int>(942, 259, mw, mh);
+Rectangle<int> ButtonControl::bankb = Rectangle<int>(1026, 260, mw, mh);
+Rectangle<int> ButtonControl::bankc = Rectangle<int>(1110, 257, mw, mh);
+Rectangle<int> ButtonControl::bankd = Rectangle<int>(1192, 258, mw, mh);
+Rectangle<int> ButtonControl::fulllevel = Rectangle<int>(777, 178, mw, mh);
+Rectangle<int> ButtonControl::sixteenlevels = Rectangle<int>(861, 180, mw, mh);
+Rectangle<int> ButtonControl::nextseq = Rectangle<int>(778, 263, mw, mh);
+Rectangle<int> ButtonControl::trackmute = Rectangle<int>(860, 261, mw, mh);
+Rectangle<int> ButtonControl::left = Rectangle<int>(416, 581, 29, 76);
+Rectangle<int> ButtonControl::up = Rectangle<int>(445, 581, 46, 38);
+Rectangle<int> ButtonControl::down = Rectangle<int>(445, 619, 46, 38);
+Rectangle<int> ButtonControl::right = Rectangle<int>(491, 581, 29, 76);
 
 void ButtonControl::initRects()
 {
@@ -99,22 +98,33 @@ void ButtonControl::initRects()
     rects.emplace("right", &right);
     rects.emplace("up", &up);
     rects.emplace("down", &down);
+    rects.emplace("0", &rect0);
+    rects.emplace("1", &rect1);
+    rects.emplace("2", &rect2);
+    rects.emplace("3", &rect3);
+    rects.emplace("4", &rect4);
+    rects.emplace("5", &rect5);
+    rects.emplace("6", &rect6);
+    rects.emplace("7", &rect7);
+    rects.emplace("8", &rect8);
+    rects.emplace("9", &rect9);
     
-    for (auto r : rects) {
-        auto rc = r.second;
+    for (auto r : rects)
         r.second->setY(r.second->getY() + 55);
-    }
 }
 
-void ButtonControl::setBounds() {
+void ButtonControl::setBounds()
+{
     setSize(rect.getWidth(), rect.getHeight());
     Component::setBounds(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
 }
 
-void ButtonControl::mouseDown(const MouseEvent& event) {
+void ButtonControl::mouseDown(const MouseEvent&)
+{
     button.lock()->push();
 }
 
-void ButtonControl::mouseUp(const MouseEvent& event) {
+void ButtonControl::mouseUp(const MouseEvent&)
+{
     button.lock()->release();
 }
--- src/main/gui/ButtonControl.hpp
+++ src/main/gui/ButtonControl.hpp
@@ -14,11 +14,10 @@ class ButtonControl
 {
     
 private:
-    juce::Rectangle<float> rect;
+    juce::Rectangle<int> rect;
     std::weak_ptr<mpc::hardware::Button> button;
-    bool mouseEntered = false;
     static const int smw = 30;
-    static const int smh = 15;
+    static const int smh = 18;
     static const int mw = 40;
     static const int mh = 20;
     static const int bw = 48;
@@ -26,56 +25,56 @@ private:
     
     
 public:
-    static std::unordered_map<std::string, juce::Rectangle<float>*> rects;
+    static std::unordered_map<std::string, juce::Rectangle<int>*> rects;
     static void initRects();
     
 private:
-    static juce::Rectangle<float> undoseq;
-    static juce::Rectangle<float> erase;
-    static juce::Rectangle<float> rec;
-    static juce::Rectangle<float> overdub;
-    static juce::Rectangle<float> stop;
-    static juce::Rectangle<float> play;
-    static juce::Rectangle<float> playstart;
-    static juce::Rectangle<float> mainscreen;
-    static juce::Rectangle<float> openwindow;
-    static juce::Rectangle<float> taptemponoterepeat;
-    static juce::Rectangle<float> prevstepevent;
-    static juce::Rectangle<float> nextstepevent;
-    static juce::Rectangle<float> gotoRect;
-    static juce::Rectangle<float> prevbarstart;
-    static juce::Rectangle<float> nextbarend;
-    static juce::Rectangle<float> f1;
-    static juce::Rectangle<float> f2;
-    static juce::Rectangle<float> f3;
-    static juce::Rectangle<float> f4;
-    static juce::Rectangle<float> f5;
-    static juce::Rectangle<float> f6;
-    static juce::Rectangle<float> notevariationafter;
-    static juce::Rectangle<float> rect0;
-    static juce::Rectangle<float> rect1;
-    static juce::Rectangle<float> rect2;
-    static juce::Rectangle<float> rect3;
-    static juce::Rectangle<float> rect4;
-    static juce::Rectangle<float> rect5;
-    static juce::Rectangle<float> rect6;
-    static juce::Rectangle<float> rect7;
-    static juce::Rectangle<float> rect8;
-    static juce::Rectangle<float> rect9;
-    static juce::Rectangle<float> shift;
-    static juce::Rectangle<float> enter;
-    static juce::Rectangle<float> banka;
-    static juce::Rectangle<float> bankb;
-    static juce::Rectangle<float> bankc;
-    static juce::Rectangle<float> bankd;
-    static juce::Rectangle<float> fulllevel;
-    static juce::Rectangle<float> sixteenlevels;
-    static juce::Rectangle<float> nextseq;
-    static juce::Rectangle<float> trackmute;
-    static juce::Rectangle<float> left;
-    static juce::Rectangle<float> up;
-    static juce::Rectangle<float> down;
-    static juce::Rectangle<float> right;
+    static juce::Rectangle<int> undoseq;
+    static juce::Rectangle<int> erase;
+    static juce::Rectangle<int> rec;
+    static juce::Rectangle<int> overdub;
+    static juce::Rectangle<int> stop;
+    static juce::Rectangle<int> play;
+    static juce::Rectangle<int> playstart;
+    static juce::Rectangle<int> mainscreen;
+    static juce::Rectangle<int> openwindow;
+    static juce::Rectangle<int> taptemponoterepeat;
+    static juce::Rectangle<int> prevstepevent;
+    static juce::Rectangle<int> nextstepevent;
+    static juce::Rectangle<int> gotoRect;
+    static juce::Rectangle<int> prevbarstart;
+    static juce::Rectangle<int> nextbarend;
+    static juce::Rectangle<int> f1;
+    static juce::Rectangle<int> f2;
+    static juce::Rectangle<int> f3;
+    static juce::Rectangle<int> f4;
+    static juce::Rectangle<int> f5;
+    static juce::Rectangle<int> f6;
+    static juce::Rectangle<int> notevariationafter;
+    static juce::Rectangle<int> rect0;
+    static juce::Rectangle<int> rect1;
+    static juce::Rectangle<int> rect2;
+    static juce::Rectangle<int> rect3;
+    static juce::Rectangle<int> rect4;
+    static juce::Rectangle<int> rect5;
+    static juce::Rectangle<int> rect6;
+    static juce::Rectangle<int> rect7;
+    static juce::Rectangle<int> rect8;
+    static juce::Rectangle<int> rect9;
+    static juce::Rectangle<int> shift;
+    static juce::Rectangle<int> enter;
+    static juce::Rectangle<int> banka;
+    static juce::Rectangle<int> bankb;
+    static juce::Rectangle<int> bankc;
+    static juce::Rectangle<int> bankd;
+    static juce::Rectangle<int> fulllevel;
+    static juce::Rectangle<int> sixteenlevels;
+    static juce::Rectangle<int> nextseq;
+    static juce::Rectangle<int> trackmute;
+    static juce::Rectangle<int> left;
+    static juce::Rectangle<int> up;
+    static juce::Rectangle<int> down;
+    static juce::Rectangle<int> right;
     
 public:
     void mouseDown(const juce::MouseEvent& event) override;
@@ -83,6 +82,6 @@ public:
     void setBounds();
     
 public:
-    ButtonControl(juce::Rectangle<float> rect, std::weak_ptr<mpc::hardware::Button> button);
+    ButtonControl(juce::Rectangle<int> rect, std::weak_ptr<mpc::hardware::Button> button);
     
 };
