5612c52866ff86de4dcf35e550cac61b8652018b
Miko Radley <datchonkyboi@gmail.com>
2022-06-03 19:24:26 +0100

    1st Keyboard implementation

--- src/main/vmpc-fltk.cpp
+++ src/main/vmpc-fltk.cpp
@@ -25,29 +25,44 @@ using namespace mpc::lcdgui::screens;
 using namespace mpc;
 
 void drawScreen(void* mpcPtr) {
-	mpc::Mpc* mpc = (mpc::Mpc*) mpcPtr;
-	puts("TICK");
+	mpc::Mpc* mpc = (mpc::Mpc*)mpcPtr;
+	auto ls = mpc->getLayeredScreen().lock();
+	mpc->getLayeredScreen().lock()->Draw();
 	auto pixels = mpc->getLayeredScreen().lock()->getPixels();
 	for (int y = 0; y < 60; y++) {
 		for (int x = 0; x < 248; x++) {
 			if ((*pixels)[x][y]) {
-				fl_color(FL_BLUE);
+				fl_color(FL_BLACK);
 			}
 			else {
-				fl_color(FL_WHITE);
+				fl_color(235, 243, 219);
 			}
 			fl_point(x, y);
 		}
 	}
-	//std::cout << "test loop" << std::endl;
-	Fl::repeat_timeout(1.0, drawScreen, &mpc);
+	std::cout << "test loop" << std::endl;
+	Fl::repeat_timeout(0.1, drawScreen, mpc);
+}
+
+int keyhandler(void* event, void* mpcPtr2) {
+	mpc::Mpc* mpc = (mpc::Mpc*)mpcPtr2;
+	if (event == FL_SHORTCUT) {
+		if (Fl::event_key()) {
+			std::cout << Fl::event_key() << std::endl;
+			auto mpcKeyHandle = mpc->getControls().lock()->getKeyEventHandler();
+			mpcKeyHandle.lock()->handle(KeyEvent(108), true);
+			return 1; 
+		}
+	}
+	return 0;
 }
 
 int main(int argc, char** argv) {
 	Mpc mpc;
 	mpc.init(44100, 1, 1);
-	mpc.getLayeredScreen().lock()->openScreen("sequencer");
-	Fl_Window* window = new Fl_Window(1000, 1000);
+	mpc.getControls().lock()->getKeyEventHandler();
+	//mpc.getLayeredScreen().lock()->openScreen("sequencer");
+	Fl_Window* window = new Fl_Window(800, 600);
 	Fl_Box* box = new Fl_Box(10, 10, 250, 70);
 	//window->fullscreen();
 
@@ -59,5 +74,7 @@ int main(int argc, char** argv) {
 	window->show(argc, argv);
 	//Fl::add_timeout(1.0, initialise, &mpc);
 	Fl::add_timeout(1.0, drawScreen, &mpc);
+	Fl::add_handler(keyhandler);
+	Fl::add_system_handler(keyhandler, &mpcPtr2);
 	return Fl::run();
 }
