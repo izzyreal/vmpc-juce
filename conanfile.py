59752f6407dd9fdb68c19b97665e55e3a9b9f1f0
Miko Radley <datchonkyboi@gmail.com>
2022-06-03 15:16:07 +0100

    Implementation of Screens

--- conanfile.py
+++ conanfile.py
@@ -2,15 +2,15 @@ from conans import ConanFile, CMake
 from sys import platform
 
 class Pkg(ConanFile):
-    name = "vmpc-juce"
-    description = "JUCE implementation of VMPC2000XL, an Akai MPC2000XL emulator"
+    name = "vmpc-fltk"
+    description = "FLTK implementation of VMPC2000XL, an Akai MPC2000XL emulator"
     license = "GPL-3.0"
     version = "0.1"
     settings = "os", "compiler", "arch", "build_type"
     generators = "cmake", "cmake_paths"
-    exports_sources = ["src/*", "resources/*"]
-    requires = ("juce/0.1@izmar/dev", "mpc/0.1@izmar/dev", "ctoot/0.1@izmar/dev", "moduru/0.1@izmar/dev")
-    url = "https://github.com/izzyreal/vmpc-juce.git"
+    exports_sourmpcs = ["src/*", "resources/*"]
+    requires = ("mpc/0.1@izmar/dev", "ctoot/0.1@izmar/dev", "moduru/0.1@izmar/dev")
+    url = "https://github.com/theModernSurrealist/vmpc-fltk.git"
 	
     def build(self):
         cmake = CMake(self)
--- src/main/vmpc-fltk.cpp
+++ src/main/vmpc-fltk.cpp
@@ -1,15 +1,58 @@
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include <FL/Fl_Box.H>
+#include <Fl/Fl_Image.H>
+#include <Fl/Fl_Shared_Image.H>
+#include <FL/Fl_BMP_Image.H>
+#include <Fl/Fl_RGB_Image.H>
+#include <Fl/Fl_PNG_Image.H>
+
+#include <Mpc.hpp>
+#include "mpc-screens.h"
+
+#include <lcdgui/LayeredScreen.hpp>
+#include <lcdgui/Layer.hpp>
+#include <lcdgui/Screens.hpp>
+#include <lcdgui/screens/OthersScreen.hpp>
+
+#include <gui/BasicStructs.hpp>
+#include <iostream>
+
+using namespace mpc::lcdgui;
+using namespace mpc::lcdgui::screens;
+
+void drawScreen(void*) {
+	puts("TICK");
+	mpc::Mpc mpc;
+	mpc.init(44100, 1, 1);
+	auto pixels = mpc.getLayeredScreen().lock()->getPixels();
+	for (int y = 0; y < 60; y++) {
+		for (int x = 0; x < 248; x++) {
+			if ((*pixels)[x][y]) {
+				fl_color(FL_BLUE);
+			}
+			else {
+				fl_color(FL_WHITE);
+			}
+			fl_point(x, y);
+		}
+	}
+	std::cout << "test loop" << std::endl;
+	Fl::repeat_timeout(1.0, drawScreen);
+}
 
 int main(int argc, char** argv) {
-	Fl_Window* window = new Fl_Window(340, 180);
-	Fl_Box* box = new Fl_Box(20, 40, 300, 100, "Hello, World!");
-	box->box(FL_UP_BOX);
-	box->labelfont(FL_BOLD + FL_ITALIC);
-	box->labelsize(36);
-	box->labeltype(FL_SHADOW_LABEL);
+
+	Fl_Window* window = new Fl_Window(1000, 1000);
+	Fl_Box* box = new Fl_Box(10, 10, 250, 70);
+	//window->fullscreen();
+
+	/*LCDControl* display;
+	display = new LCDControl(mpc, mpc.getLayeredScreen());
+	display->drawPixelsToImg();*/
+
 	window->end();
 	window->show(argc, argv);
+	Fl::add_timeout(1.0, drawScreen);
 	return Fl::run();
 }
