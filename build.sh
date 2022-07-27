390aa346987091dd9a4a0fc834d3df52670d2a9f
Izmar Verhage <izmaelverhage@gmail.com>
2022-07-27 19:15:12 +0200

    Eliminate git submodules

--- build.sh
+++ build.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
-cwd=$(pwd)
+
 mkdir -p build
+
+cwd=$(pwd)
+
 cmake \
   -B build \
   -G "Ninja" \
-  -DCPM_moduru_SOURCE="$cwd/moduru" \
-  -DCPM_ctoot_SOURCE="$cwd/ctoot" \
-  -DCPM_mpc_SOURCE="$cwd/mpc" \
-  -DCPM_JUCE_SOURCE="$cwd/JUCE" \
-  -DCPM_juce-raw-keyboard-input-module_SOURCE="$cwd/juce-raw-keyboard-input-module" \
+  \
+  -DCPM_mpc_SOURCE="$cwd/editables/mpc" \
+  -DCPM_akaifat_SOURCE="$cwd/editables/akaifat" \
+  -DCPM_moduru_SOURCE="$cwd/editables/moduru" \
+  -DCPM_ctoot_SOURCE="$cwd/editables/ctoot" \
+  -DCPM_juce-raw-keyboard-input-module_SOURCE="$cwd/editables/juce-raw-keyboard-input-module" \
+  \
+  -DCPM_JUCE_SOURCE="$cwd/deps/JUCE" \
+  -DCPM_rapidjson_SOURCE="$cwd/deps/rapidjson/include" \
+  -DCPM_Catch2_SOURCE="$cwd/deps/Catch2" \
+  -DCPM_filesystem_SOURCE="$cwd/deps/filesystem" \
+  \
+  -Dexpected_SOURCE_DIR="$cwd/deps/expected" \
+  \
   -DCMAKE_BUILD_TYPE="Release"
-ninja vmpc2000xl_All
-ninja -C build vmpc2000xl_Standalone
+
+ninja -C build vmpc2000xl_All
