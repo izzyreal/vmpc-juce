30677faa2df43de2f3fc8755243180b867caf1ac
Izmar Verhage <izmaelverhage@gmail.com>
2022-09-08 09:01:37 +0200

    MPC-79 Update pressedPadVelos when dragging a pad

--- src/main/gui/PadControl.cpp
+++ src/main/gui/PadControl.cpp
@@ -30,24 +30,27 @@ using namespace mpc::lcdgui::screens::window;
 using namespace mpc::lcdgui::screens::dialog2;
 using namespace moduru::lang;
 
-PadControl::PadControl(mpc::Mpc& _mpc, juce::Rectangle<float> _rect, std::weak_ptr<mpc::hardware::HwPad> _pad, Image _padHitImg)
-: mpc(_mpc), pad(_pad), padhitImg (_padHitImg), rect (_rect)
+PadControl::PadControl(mpc::Mpc &_mpc, juce::Rectangle<float> _rect, std::weak_ptr<mpc::hardware::HwPad> _pad,
+                       Image _padHitImg)
+        : mpc(_mpc), pad(_pad), padhitImg(_padHitImg), rect(_rect)
 {
     pad.lock()->addObserver(this);
 }
 
-bool PadControl::isInterestedInFileDrag(const StringArray& files)
+bool PadControl::isInterestedInFileDrag(const StringArray &files)
 {
     if (files.size() != 1)
     {
         return false;
     }
-    
-    for (auto& s : files)
+
+    for (auto &s: files)
     {
-        if (StrUtil::hasEnding(StrUtil::toLower(s.toStdString()), ".snd") || StrUtil::hasEnding(StrUtil::toLower(s.toStdString()), ".wav"))
+        if (StrUtil::hasEnding(StrUtil::toLower(s.toStdString()), ".snd") ||
+            StrUtil::hasEnding(StrUtil::toLower(s.toStdString()), ".wav"))
         {
-            if (padhitBrightness == 0) {
+            if (padhitBrightness == 0)
+            {
                 fading = true;
                 padhitBrightness = 152;
                 repaint();
@@ -61,107 +64,113 @@ bool PadControl::isInterestedInFileDrag(const StringArray& files)
 
 void PadControl::loadFile(const String path, bool shouldBeConverted, std::string screenToReturnTo)
 {
-    if (StrUtil::hasEnding(StrUtil::toLower(path.toStdString()), ".snd") || StrUtil::hasEnding(StrUtil::toLower(path.toStdString()), ".wav"))
+    if (StrUtil::hasEnding(StrUtil::toLower(path.toStdString()), ".snd") ||
+        StrUtil::hasEnding(StrUtil::toLower(path.toStdString()), ".wav"))
     {
         auto sampler = mpc.getSampler().lock();
-        
+
         auto soundLoader = SoundLoader(mpc, sampler->getSounds(), false);
         soundLoader.setPreview(false);
-        
+
         auto compatiblePath = StrUtil::replaceAll(path.toStdString(), '\\', std::string("\\"));
-        
+
         auto file = std::make_shared<mpc::disk::MpcFile>(fs::path(compatiblePath));
-        
+
         auto layeredScreen = mpc.getLayeredScreen().lock();
-        
+
         SoundLoaderResult result;
-        
+
         try
         {
             soundLoader.loadSound(file, result, shouldBeConverted);
         }
-        catch (const std::exception& exception)
+        catch (const std::exception &exception)
         {
             MLOG("A problem occurred when trying to load " + compatiblePath + ": " + std::string(exception.what()));
             MLOG(result.errorMessage);
             layeredScreen->openScreen(layeredScreen->getPreviousScreenName());
             return;
         }
-        
+
         auto popupScreen = mpc.screens->get<PopupScreen>("popup");
 
         if (!result.success)
         {
             if (result.soundWasAdded) sampler->deleteSound(sampler->getPreviewSound());
-            
-            if (result.canBeConverted) {
+
+            if (result.canBeConverted)
+            {
                 auto loadRoutine = [&, path, screenToReturnTo, layeredScreen]() {
                     const bool shouldBeConverted2 = true;
                     loadFile(path, shouldBeConverted2, screenToReturnTo);
                 };
-                
-                auto convertAndLoadWavScreen = mpc.screens->get<VmpcConvertAndLoadWavScreen>("vmpc-convert-and-load-wav");
+
+                auto convertAndLoadWavScreen = mpc.screens->get<VmpcConvertAndLoadWavScreen>(
+                        "vmpc-convert-and-load-wav");
                 convertAndLoadWavScreen->setLoadRoutine(loadRoutine);
                 layeredScreen->openScreen("vmpc-convert-and-load-wav");
-            } else {
+            }
+            else
+            {
                 layeredScreen->openScreen("popup");
                 popupScreen->setText(result.errorMessage);
                 popupScreen->returnToScreenAfterMilliSeconds("load", 500);
             }
-            
+
             return;
         }
-        
+
         if (result.existingIndex == -1)
         {
             auto soundFileName = StrUtil::toUpper(file->getNameWithoutExtension());
             if (soundFileName.length() >= 16) soundFileName = soundFileName.substr(0, 16);
-            
+
             auto ext = file->getExtension();
-            
+
             popupScreen->setText("LOADING " + StrUtil::padRight(soundFileName, " ", 16) + "." + ext);
-            
+
             layeredScreen->openScreen("popup");
             popupScreen->returnToScreenAfterMilliSeconds(screenToReturnTo, 300);
-            
+
             auto drumIndex = mpc.getSequencer().lock()->getActiveTrack().lock()->getBus() - 1;
-            
+
             if (drumIndex == -1)
             {
                 layeredScreen->openScreen(screenToReturnTo);
                 return;
             }
-            
+
             auto mpcSoundPlayerChannel = mpc.getDrum(drumIndex);
-            
+
             auto programIndex = mpcSoundPlayerChannel->getProgram();
             auto program = mpc.getSampler().lock()->getProgram(programIndex).lock();
             auto soundIndex = mpc.getSampler().lock()->getSoundCount() - 1;
             auto padIndex = pad.lock()->getIndex() + (mpc.getBank() * 16);
             auto programPad = program->getPad(padIndex);
             auto padNote = programPad->getNote();
-            
-            auto noteParameters = dynamic_cast<mpc::sampler::NoteParameters*>(program->getNoteParameters(padNote));
-            
+
+            auto noteParameters = dynamic_cast<mpc::sampler::NoteParameters *>(program->getNoteParameters(padNote));
+
             if (noteParameters == nullptr)
             {
                 layeredScreen->openScreen(layeredScreen->getPreviousScreenName());
                 return;
             }
-            
+
             noteParameters->setSoundIndex(soundIndex);
         }
     }
 }
 
-void PadControl::filesDropped(const StringArray& files, int, int)
+void PadControl::filesDropped(const StringArray &files, int, int)
 {
     if (files.size() != 1) return;
-    
+
     const bool shouldBeConverted = false;
     std::string screenToReturnTo = mpc.getLayeredScreen().lock()->getCurrentScreenName();
-    
-    for (auto& f : files) {
+
+    for (auto &f: files)
+    {
         loadFile(f, shouldBeConverted, screenToReturnTo);
     }
 }
@@ -172,7 +181,7 @@ void PadControl::timerCallback()
     {
         padhitBrightness -= 20;
     }
-    
+
     if (padhitBrightness < 0)
     {
         padhitBrightness = 0;
@@ -186,10 +195,10 @@ void PadControl::timerCallback()
     }
 }
 
-void PadControl::update(moduru::observer::Observable*, nonstd::any arg)
+void PadControl::update(moduru::observer::Observable *, nonstd::any arg)
 {
     int velocity = nonstd::any_cast<int>(arg);
-    
+
     if (velocity == 255)
     {
         fading = true;
@@ -218,18 +227,30 @@ int PadControl::getVelo(int x, int y)
     return velo;
 }
 
-void PadControl::mouseDown(const MouseEvent& event)
+void PadControl::mouseDown(const MouseEvent &event)
 {
     pad.lock()->push(getVelo(event.x, event.y));
 }
 
-void PadControl::mouseDoubleClick(const MouseEvent&)
+void PadControl::mouseDoubleClick(const MouseEvent &)
 {
 }
 
-void PadControl::mouseUp(const MouseEvent&)
+void PadControl::mouseUp(const MouseEvent &)
 {
     pad.lock()->release();
+}
+
+void PadControl::mouseDrag(const MouseEvent &event)
+{
+    auto controls = mpc.getControls().lock();
+    auto padIndex = pad.lock()->getIndex();
+    if (controls->getPressedPads()->find(padIndex) == controls->getPressedPads()->end())
+        return;
+
+    auto newVelo = getVelo(event.x, event.y);
+
+    (*controls->getPressedPadVelos())[padIndex] = newVelo;
 }
 
 void PadControl::setBounds()
@@ -238,10 +259,10 @@ void PadControl::setBounds()
     Component::setBounds(rect.getX(), rect.getY(), rect.getWidth(), rect.getHeight());
 }
 
-void PadControl::paint(Graphics& g)
+void PadControl::paint(Graphics &g)
 {
     auto img = padhitImg.createCopy();
-    float mult = (float)(padhitBrightness) / 150.0;
+    float mult = (float) (padhitBrightness) / 150.0;
     img.multiplyAllAlphas(mult);
     g.drawImageAt(img, 0, 0);
 }
@@ -250,4 +271,3 @@ PadControl::~PadControl()
 {
     pad.lock()->deleteObserver(this);
 }
-
--- src/main/gui/PadControl.hpp
+++ src/main/gui/PadControl.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include "VmpcComponent.h"
 
 #include <observer/Observer.hpp>
@@ -8,45 +9,45 @@
 
 namespace mpc { class Mpc; }
 
-namespace mpc::hardware
-{
-	class HwPad;
+namespace mpc::hardware {
+    class HwPad;
 }
 
 class PadControl
-	: public VmpcComponent
-	, public juce::Timer
-	, public juce::FileDragAndDropTarget
-	, public moduru::observer::Observer
+        : public VmpcComponent,
+          public juce::Timer,
+          public juce::FileDragAndDropTarget,
+          public moduru::observer::Observer
 {
 
 private:
-    mpc::Mpc& mpc;
+    mpc::Mpc &mpc;
     std::weak_ptr<mpc::hardware::HwPad> pad;
     juce::Image padhitImg;
-    juce::Rectangle <float> rect;
+    juce::Rectangle<float> rect;
 
     bool fading = false;
-	bool pressed = false;
-	int padhitBrightness = 0;
+    bool pressed = false;
+    int padhitBrightness = 0;
 
     int getVelo(int x, int y);
     void loadFile(const juce::String path, bool shouldBeConverted, std::string screenToReturnTo);
-    
-public:
-	void paint(juce::Graphics& g) override;
-	void mouseDown(const juce::MouseEvent& event) override;
-	void mouseUp(const juce::MouseEvent& event) override;
-	void mouseDoubleClick(const juce::MouseEvent& event) override;
-	void timerCallback() override;
-    bool isInterestedInFileDrag(const juce::StringArray& files) override;
-    void filesDropped(const juce::StringArray& files, int x, int y) override;
 
 public:
-	void update(moduru::observer::Observable* o, nonstd::any arg) override;
-	void setBounds();
+    void paint(juce::Graphics &g) override;
+    void mouseDown(const juce::MouseEvent &event) override;
+    void mouseUp(const juce::MouseEvent &event) override;
+    void mouseDrag(const juce::MouseEvent &event) override;
+    void mouseDoubleClick(const juce::MouseEvent &event) override;
+    void timerCallback() override;
+    bool isInterestedInFileDrag(const juce::StringArray &files) override;
+    void filesDropped(const juce::StringArray &files, int x, int y) override;
 
 public:
-	PadControl(mpc::Mpc& mpc, juce::Rectangle<float> rect, std::weak_ptr<mpc::hardware::HwPad> pad, juce::Image padhit);
-	~PadControl() override;
+    void update(moduru::observer::Observable *o, nonstd::any arg) override;
+    void setBounds();
+
+public:
+    PadControl(mpc::Mpc &mpc, juce::Rectangle<float> rect, std::weak_ptr<mpc::hardware::HwPad> pad, juce::Image padhit);
+    ~PadControl() override;
 };
