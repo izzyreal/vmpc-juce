b3095f08fac73990d75b611db7e8987305109389
Izmar Verhage <izmaelverhage@gmail.com>
2022-10-23 12:56:29 +0200

    Add resizable aux window with duplicate LCD contents

--- src/main/gui/LCDControl.cpp
+++ src/main/gui/LCDControl.cpp
@@ -10,8 +10,8 @@
 using namespace mpc::lcdgui;
 using namespace mpc::lcdgui::screens;
 
-LCDControl::LCDControl(mpc::Mpc& _mpc, std::weak_ptr<mpc::lcdgui::LayeredScreen> _ls)
-	: mpc (_mpc), ls (_ls)
+LCDControl::LCDControl(mpc::Mpc& _mpc)
+	: mpc (_mpc), ls (_mpc.getLayeredScreen().lock())
 {
 	lcd = juce::Image(juce::Image::RGB, 496, 120, true);
 	auto othersScreen = mpc.screens->get<OthersScreen>("others");
@@ -22,16 +22,16 @@ void LCDControl::update(moduru::observer::Observable*, nonstd::any msg)
 {
 	auto message = nonstd::any_cast<std::string>(msg);
 
-	if (message.compare("contrast") == 0)
+	if (message == "contrast")
 	{
-		ls.lock()->getFocusedLayer().lock()->SetDirty(); // Could be done less invasively by just redrawing the current pixels of the LCD screens, but with updated colors
+		ls->getFocusedLayer().lock()->SetDirty(); // Could be done less invasively by just redrawing the current pixels of the LCD screens, but with updated colors
 		repaint();
 	}
 }
 
 void LCDControl::drawPixelsToImg()
 {
-	auto pixels = ls.lock()->getPixels();
+	auto pixels = ls->getPixels();
 	
 	auto othersScreen = mpc.screens->get<OthersScreen>("others");
 	auto contrast = othersScreen->getContrast();
@@ -41,6 +41,8 @@ void LCDControl::drawPixelsToImg()
 	auto halfOn = Constants::LCD_HALF_ON.darker(static_cast<float>(contrast * 0.02));
   auto on = Constants::LCD_ON.darker(static_cast<float>(contrast * 0.02));
 	auto off = Constants::LCD_OFF.brighter(static_cast<float>(contrast * 0.01428));
+
+    if (isAux) dirtyRect = juce::Rectangle<int>(248, 60);
 
 	const auto rectX = dirtyRect.getX();
 	const auto rectY = dirtyRect.getY();
@@ -72,15 +74,24 @@ void LCDControl::drawPixelsToImg()
 	dirtyRect = juce::Rectangle<int>();
 }
 
+bool LCDControl::auxNeedsToUpdate = false;
+
 void LCDControl::checkLsDirty()
 {
-	if (ls.lock()->IsDirty())
+    if (isAux && auxNeedsToUpdate)
+    {
+        drawPixelsToImg();
+        repaint();
+        auxNeedsToUpdate = false;
+    }
+	else if (!isAux && ls->IsDirty())
 	{
-		auto dirtyArea = ls.lock()->getDirtyArea();
+		auto dirtyArea = ls->getDirtyArea();
 		dirtyRect = juce::Rectangle<int>(dirtyArea.L, dirtyArea.T, dirtyArea.W(), dirtyArea.H());
-		ls.lock()->Draw();
+		ls->Draw();
 		drawPixelsToImg();
 		repaint();
+        auxNeedsToUpdate = true;
 	}
 }
 
@@ -91,11 +102,61 @@ void LCDControl::timerCallback()
 
 void LCDControl::paint(juce::Graphics& g)
 {
-	g.drawImageAt(lcd, 0, 0);
+    if (isAux)
+    {
+        g.drawImage(lcd, getLocalBounds().toFloat());
+    }
+	else
+    {
+        g.drawImageAt(lcd, 0, 0);
+    }
+}
+
+void LCDControl::mouseDoubleClick (const juce::MouseEvent&)
+{
+    if (isAux)
+    {
+        return;
+    }
+
+    if (auxWindow != nullptr)
+    {
+        delete auxWindow;
+        auxWindow = nullptr;
+    }
+    else
+    {
+        auxWindow = new juce::ResizableWindow("foo", true);
+        auxWindow->setOpaque(false);
+        auxWindow->setVisible (true);
+        const int margin = 20;
+        auxWindow->setResizable(true, true);
+        auxWindow->setResizeLimits(248 + margin, 60 + margin, (248*8) + margin, (60 * 8) + margin);
+        auxWindow->getConstrainer()->setFixedAspectRatio((496.f + margin) / (120.f + margin));
+        auxWindow->setBounds(0, 0, 496 + margin, 120 + margin);
+
+        class AuxLCD : public LCDControl {
+        public: AuxLCD(mpc::Mpc& m) : LCDControl(m) {}
+            void resized() override {
+                setBounds(margin / 2, margin / 2, getParentWidth() - margin, getParentHeight() - margin);
+            }
+        };
+
+        auto auxLcd = new AuxLCD(mpc);
+        auxLcd->isAux = true;
+        auxWindow->setContentOwned(auxLcd, false);
+        auxLcd->drawPixelsToImg();
+        auxLcd->startTimer(25);
+    }
 }
 
 LCDControl::~LCDControl()
 {
   auto othersScreen = mpc.screens->get<OthersScreen>("others");
   othersScreen->deleteObserver(this);
+
+  if (auxWindow != nullptr)
+  {
+      delete auxWindow;
+  }
 }
--- src/main/gui/LCDControl.h
+++ src/main/gui/LCDControl.h
@@ -19,16 +19,20 @@ class LCDControl
 {
 
 private:
-	mpc::Mpc& mpc;
-	std::weak_ptr<mpc::lcdgui::LayeredScreen> ls;
+    bool isAux = false;
+    juce::ResizableWindow* auxWindow = nullptr;
+    mpc::Mpc& mpc;
+	std::shared_ptr<mpc::lcdgui::LayeredScreen> ls;
 	juce::Image lcd;
     juce::Rectangle<int> dirtyRect;
+    static bool auxNeedsToUpdate;
 
 public:
 	void checkLsDirty();
 	void drawPixelsToImg();
 	void paint(juce::Graphics& g) override;
 	void timerCallback() override;
+    void mouseDoubleClick (const juce::MouseEvent&) override;
   void mouseDown(const juce::MouseEvent& e) override {
     getParentComponent()->mouseDown(e);
   }
@@ -37,7 +41,7 @@ public:
   }
 
 public:
-	LCDControl(mpc::Mpc& mpc, std::weak_ptr<mpc::lcdgui::LayeredScreen> ls);
+	LCDControl(mpc::Mpc& mpc);
   ~LCDControl() override;
 	void update(moduru::observer::Observable* o, nonstd::any msg) override;
 
