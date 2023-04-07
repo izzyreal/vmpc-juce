b43b051dbdeb27315c4a5e1f2ff297bde576efdb
Izmar Verhage <izmaelverhage@gmail.com>
2023-04-07 09:44:27 -0500

    Update build-ios.sh

--- build-ios.sh
+++ build-ios.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
 cmake -B build -G Xcode -D CMAKE_SYSTEM_NAME="iOS"
+echo 'Your iOS build is ready. Open vmpc-juce/build/vmpc2000xl.xcodeproj in Xcode and select the vmpc2000xl_Standalone target. If it's not there, add it via Manage Schemes.'
