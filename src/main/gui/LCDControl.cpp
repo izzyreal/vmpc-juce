9547694d7c5e06683bc9f137a192ab3d970da431
Izmar Verhage <izmaelverhage@gmail.com>
2023-10-07 14:25:55 +0200

    Use avir for resizing the LCD bitmap

--- src/main/gui/LCDControl.cpp
+++ src/main/gui/LCDControl.cpp
@@ -11,8 +11,55 @@
 
 #include <raw_keyboard_input/src/Keyboard.h>
 
+#include "avir.h"
+
 using namespace mpc::lcdgui;
 using namespace mpc::lcdgui::screens;
+
+juce::Image applyResize (const juce::Image& src, int width, int height)
+{
+    juce::Image dst (src.getFormat(), width, height, true);
+
+    juce::Image::BitmapData srcData (src, juce::Image::BitmapData::readOnly);
+    juce::Image::BitmapData dstData (dst, juce::Image::BitmapData::readWrite);
+
+    int channels = 0;
+    if (src.getFormat() == juce::Image::ARGB)               channels = 4;
+    else if (src.getFormat() == juce::Image::RGB)           channels = 3;
+    else if (src.getFormat() == juce::Image::SingleChannel) channels = 1;
+    else                                                    return {};
+
+    // JUCE images may have padding at the end of each scan line.
+    // Avir expects the image data to be packed. So we need to
+    // pack and unpack the image data before and after resizing.
+    juce::HeapBlock<uint8_t> srcPacked (src.getWidth() * src.getHeight() * channels);
+    juce::HeapBlock<uint8_t> dstPacked (dst.getWidth() * dst.getHeight() * channels);
+
+    uint8_t* rawSrc = srcPacked.getData();
+    uint8_t* rawDst = dstPacked.getData();
+
+    for (int y = 0; y < src.getHeight(); y++)
+        memcpy (rawSrc + y * src.getWidth() * channels,
+               srcData.getLinePointer (y),
+               (size_t) (src.getWidth() * channels));
+
+#if USE_SSE
+    avir::CImageResizer<avir::fpclass_float4> imageResizer (8);
+    imageResizer.resizeImage (rawSrc, src.getWidth(), src.getHeight(), 0,
+                             rawDst, dst.getWidth(), dst.getHeight(), channels, 0);
+#else
+    avir::CImageResizer<> imageResizer (8);
+    imageResizer.resizeImage (rawSrc, src.getWidth(), src.getHeight(), 0,
+                             rawDst, dst.getWidth(), dst.getHeight(), channels, 0);
+#endif
+
+    for (int y = 0; y < dst.getHeight(); y++)
+        memcpy (dstData.getLinePointer (y),
+               rawDst + y * dst.getWidth() * channels,
+               (size_t) (dst.getWidth() * channels));
+
+    return dst;
+}
 
 LCDControl::LCDControl(mpc::Mpc& _mpc)
     : mpc(_mpc), ls(_mpc.getLayeredScreen())
@@ -116,9 +163,21 @@ void LCDControl::timerCallback()
     checkLsDirty();
 }
 
+void paintRescaledImage(juce::Graphics& g, juce::Rectangle<int> src, juce::Rectangle<int> dest, juce::Image originalImgToDraw)
+{
+    const auto pxFactor = g.getInternalContext().getPhysicalPixelScaleFactor();
+    const int width = juce::roundToInt(pxFactor*dest.getWidth());
+    const int height = juce::roundToInt(pxFactor*dest.getHeight());
+    auto cutImage = originalImgToDraw.getClippedImage(src);
+    auto finalImgToDraw = applyResize(cutImage, width, height);
+    juce::Graphics::ScopedSaveState ph(g);
+    g.setImageResamplingQuality(juce::Graphics::lowResamplingQuality);
+    g.drawImageTransformed(finalImgToDraw, juce::AffineTransform::scale(1.f/pxFactor).translated(dest.getX(), dest.getY()));
+}
+
 void LCDControl::paint(juce::Graphics &g)
 {
-    g.drawImageAt(lcd, 0, 0);
+    paintRescaledImage(g, lcd.getBounds(), getLocalBounds(), lcd);
 }
 
 void LCDControl::mouseDoubleClick(const juce::MouseEvent &)
