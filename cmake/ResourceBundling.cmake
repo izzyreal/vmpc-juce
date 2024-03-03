0902aac9ca90931c017c822bbe8186cf2a963d5b
Izmar Verhage <izmaelverhage@gmail.com>
2024-03-03 14:39:22 +0100

    Use glob in resource bundling and exclude .DS_Store

--- cmake/ResourceBundling.cmake
+++ cmake/ResourceBundling.cmake
@@ -3,11 +3,9 @@ include(cmake/CMakeRC.cmake)
 set(_vmpc_juce_resources_root ${CMAKE_CURRENT_SOURCE_DIR}/resources)
 
 function(_bundle_vmpc_juce_resources _target_name)
-  set(VMPC_JUCE_RESOURCES "")
 
-  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img jpg "${VMPC_JUCE_RESOURCES}")
-  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img png "${VMPC_JUCE_RESOURCES}")
-  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img gif "${VMPC_JUCE_RESOURCES}")
+  file(GLOB_RECURSE VMPC_JUCE_RESOURCES "${_vmpc_juce_resources_root}/*")
+  list(FILTER VMPC_JUCE_RESOURCES EXCLUDE REGEX "\\.DS_Store$")
 
   if (APPLE)
     set_source_files_properties(${VMPC_JUCE_RESOURCES} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/img)
@@ -17,8 +15,8 @@ function(_bundle_vmpc_juce_resources _target_name)
     target_sources(vmpc2000xl_VST3 PRIVATE ${VMPC_JUCE_RESOURCES})
 
     file(GLOB_RECURSE MPC_RESOURCES "${CMAKE_SOURCE_DIR}/editables/mpc/resources/*")
-
     list(FILTER MPC_RESOURCES EXCLUDE REGEX "${CMAKE_SOURCE_DIR}/editables/mpc/resources/test/.*")
+    list(FILTER MPC_RESOURCES EXCLUDE REGEX "\\.DS_Store$")
 
     foreach(RESOURCE ${MPC_RESOURCES})
         get_filename_component(SOURCE_DIR "${RESOURCE}" DIRECTORY)
@@ -39,16 +37,4 @@ function(_bundle_vmpc_juce_resources _target_name)
     )
     target_link_libraries(${_target_name} PUBLIC vmpcjuce::rc)
   endif()
-endfunction()
-
-function(_add_resource_files _target_name _rsrc_root_path _sub_dir _extension _VMPC_JUCE_RESOURCES)
-  file(
-    GLOB _list
-    LIST_DIRECTORIES false
-    RELATIVE "${CMAKE_CURRENT_LIST_DIR}"
-    "${_rsrc_root_path}/${_sub_dir}/*.${_extension}"
-    )
-
-  list (APPEND _VMPC_JUCE_RESOURCES ${_list})
-  set (VMPC_JUCE_RESOURCES ${_VMPC_JUCE_RESOURCES} PARENT_SCOPE)
 endfunction()
