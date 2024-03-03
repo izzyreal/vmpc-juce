516c9f8c2a65875b768b17115ba074071c96a8fa
Izmar Verhage <izmaelverhage@gmail.com>
2024-03-03 13:25:43 +0100

    Remove unnecessary set_target_properties call
    Also reorder the cmake resource code a bit

--- cmake/ResourceBundling.cmake
+++ cmake/ResourceBundling.cmake
@@ -3,13 +3,18 @@ include(cmake/CMakeRC.cmake)
 set(_vmpc_juce_resources_root ${CMAKE_CURRENT_SOURCE_DIR}/resources)
 
 function(_bundle_vmpc_juce_resources _target_name)
-  set(total_list "")
+  set(VMPC_JUCE_RESOURCES "")
 
-  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img jpg "${total_list}")
-  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img png "${total_list}")
-  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img gif "${total_list}")
+  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img jpg "${VMPC_JUCE_RESOURCES}")
+  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img png "${VMPC_JUCE_RESOURCES}")
+  _add_resource_files(${_target_name} ${_vmpc_juce_resources_root} img gif "${VMPC_JUCE_RESOURCES}")
 
   if (APPLE)
+    set_source_files_properties(${VMPC_JUCE_RESOURCES} PROPERTIES MACOSX_PACKAGE_LOCATION Resources/img)
+
+    target_sources(vmpc2000xl_AU PRIVATE ${VMPC_JUCE_RESOURCES})
+    target_sources(vmpc2000xl_AUv3 PRIVATE ${VMPC_JUCE_RESOURCES})
+    target_sources(vmpc2000xl_VST3 PRIVATE ${VMPC_JUCE_RESOURCES})
 
     file(GLOB_RECURSE MPC_RESOURCES "${CMAKE_SOURCE_DIR}/editables/mpc/resources/*")
 
@@ -24,33 +29,19 @@ function(_bundle_vmpc_juce_resources _target_name)
     target_sources(vmpc2000xl_AU PRIVATE ${MPC_RESOURCES})
     target_sources(vmpc2000xl_AUv3 PRIVATE ${MPC_RESOURCES})
     target_sources(vmpc2000xl_VST3 PRIVATE ${MPC_RESOURCES})
-
-    set_source_files_properties(
-            ${total_list}
-            PROPERTIES
-            MACOSX_PACKAGE_LOCATION Resources/img
-    )
-
-    target_sources(vmpc2000xl_AU PRIVATE ${total_list})
-    target_sources(vmpc2000xl_AUv3 PRIVATE ${total_list})
-    target_sources(vmpc2000xl_VST3 PRIVATE ${total_list})
-
-    string(REPLACE "resources" "Resources" total_list_upper_case ${total_list})
-
-    set_target_properties(vmpc2000xl PROPERTIES RESOURCE ${total_list_upper_case})
   else()
     cmrc_add_resource_library(
             vmpc_juce_resources
             ALIAS vmpcjuce::rc
             NAMESPACE vmpcjuce
             WHENCE ${_vmpc_juce_resources_root}
-            ${total_list}
+            ${VMPC_JUCE_RESOURCES}
     )
     target_link_libraries(${_target_name} PUBLIC vmpcjuce::rc)
   endif()
 endfunction()
 
-function(_add_resource_files _target_name _rsrc_root_path _sub_dir _extension _total_list)
+function(_add_resource_files _target_name _rsrc_root_path _sub_dir _extension _VMPC_JUCE_RESOURCES)
   file(
     GLOB _list
     LIST_DIRECTORIES false
@@ -58,6 +49,6 @@ function(_add_resource_files _target_name _rsrc_root_path _sub_dir _extension _t
     "${_rsrc_root_path}/${_sub_dir}/*.${_extension}"
     )
 
-  list (APPEND _total_list ${_list})
-  set (total_list ${_total_list} PARENT_SCOPE)
+  list (APPEND _VMPC_JUCE_RESOURCES ${_list})
+  set (VMPC_JUCE_RESOURCES ${_VMPC_JUCE_RESOURCES} PARENT_SCOPE)
 endfunction()
