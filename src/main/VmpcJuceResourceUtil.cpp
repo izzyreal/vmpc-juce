5e251290d7c3f3a73d2968114256f51b66240d3b
Izmar Verhage <izmaelverhage@gmail.com>
2024-12-03 12:27:10 +0100

    Fix resource loading on Windows

--- src/main/VmpcJuceResourceUtil.cpp
+++ src/main/VmpcJuceResourceUtil.cpp
@@ -2,6 +2,7 @@
 
 #ifdef MAC_BUNDLE_RESOURCES
 #include "MacBundleResources.h"
+#include "mpc_fs.hpp"
 #else
 #include <cmrc/cmrc.hpp>
 #include <string_view>
@@ -9,6 +10,15 @@ CMRC_DECLARE(vmpcjuce);
 #endif
 
 using namespace vmpc_juce;
+
+std::vector<char> VmpcJuceResourceUtil::getResourceData(const std::string& path)
+{
+#ifdef MAC_BUNDLE_RESOURCES
+    return getResourceDataFromMacBundleResources(path);
+#else
+    return getResourceDataFromInMemoryFS(path);
+#endif
+}
 
 juce::Image VmpcJuceResourceUtil::loadImage(const std::string& path)
 {
@@ -25,13 +35,26 @@ juce::Image VmpcJuceResourceUtil::loadImageFromMacBundleResources(const std::str
     const auto imgPath = mpc::MacBundleResources::getResourcePath(path);
     return juce::ImageFileFormat::loadFrom(juce::File(imgPath));
 }
+
+std::vector<char> VmpcJuceResourceUtil::getResourceDataFromMacBundleResources(const std::string& path)
+{
+    const auto resource_path = mpc::MacBundleResources::getResourcePath(path);
+    return get_file_data(resource_path);
+}
 #else
+
+std::vector<char> VmpcJuceResourceUtil::getResourceDataFromInMemoryFS(const std::string& path)
+{
+    const auto file = cmrc::vmpcjuce::get_filesystem().open(path.c_str());
+    const auto data = std::string_view(file.begin(), file.size()).data();
+    return { data, data + file.size() };
+}
+
 juce::Image VmpcJuceResourceUtil::loadImageFromInMemoryFS(const std::string& path)
 {
-  auto fs = cmrc::vmpcjuce::get_filesystem();
-  auto file = fs.open(path.c_str());
-  auto data = std::string_view(file.begin(), file.size()).data();
-  auto stream = juce::MemoryInputStream(data, file.size(), true);
-  return juce::ImageFileFormat::loadFrom(stream);
+    const auto file = cmrc::vmpcjuce::get_filesystem().open(path.c_str());
+    const auto data = std::string_view(file.begin(), file.size()).data();
+    auto stream = juce::MemoryInputStream(data, file.size(), true);
+    return juce::ImageFileFormat::loadFrom(stream);
 }
 #endif
