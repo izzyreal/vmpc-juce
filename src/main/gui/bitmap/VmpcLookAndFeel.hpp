c02dde04c97019e27ae713684a5f2a8768d8590a
Izmar Verhage <izmaelverhage@gmail.com>
2024-12-08 17:37:22 +0100

    Minimize 4

--- src/main/gui/bitmap/VmpcLookAndFeel.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include "juce_gui_basics/juce_gui_basics.h"
-
-class VmpcLookAndFeel : public juce::LookAndFeel_V4 {
-
-public:
-    void drawCornerResizer (juce::Graphics&, int, int, bool, bool) override {}
-};
