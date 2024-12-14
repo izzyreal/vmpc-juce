4e582aa30cf89e71c5cfe0d554d4e63ea8528742
Izmar Verhage <izmaelverhage@gmail.com>
2024-12-14 19:03:15 +0100

    Improve initial dimensions

---
+++ src/main/InitialWindowDimensions.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <cstdint>
+#include <utility>
+
+#include <juce_gui_basics/juce_gui_basics.h>
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+#elif defined(__linux__)
+#include <X11/Xlib.h>
+#include <X11/extensions/Xrandr.h>
+#endif
+
+#ifdef __APPLE__
+uint32_t getInitialWindowHeightApple();
+#endif
+
+namespace vmpc_juce {
+
+    class InitialWindowDimensions {
+        public:
+            static std::pair<int, int> get()
+            {
+                const int safeW = 445, safeH = 342;
+
+                int w, h;
+
+                if (juce::JUCEApplication::isStandaloneApp())
+                {
+                    h = getInitialWindowHeight();
+
+                    if (h == 0)
+                    {
+                        w = safeW; h = safeH;
+                    }
+                    else
+                    {
+                        w = h * (safeW / (float) safeH);
+                    }
+                }
+                else
+                {
+                    w = safeW; h = safeH;
+                }
+
+                return { w, h };
+            }
+
+        private:
+            static uint32_t getInitialWindowHeight()
+            {
+#ifdef __APPLE__
+                return getInitialWindowHeightApple();
+#elif defined(_WIN32) || defined(_WIN64)
+                RECT workArea;
+                SystemParametersInfo(SPI_GETWORKAREA, 0, &workArea, 0);
+                return workArea.bottom - workArea.top - GetSystemMetrics(SM_CYCAPTION) - (2 * GetSystemMetrics(SM_CYFRAME));
+#elif defined(__linux__)
+                Display* display = XOpenDisplay(nullptr);
+
+                if (!display)
+                {
+                    return 0;
+                }
+
+                int32_t height = DisplayHeight(display, DefaultScreen(display));
+                XCloseDisplay(display);
+                return height;
+#else
+                return 0;
+#endif
+            }
+    };
+} // namespace vmpc_juce
