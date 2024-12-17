0d330a1c188ebc1cc8cd8f8820a9092144685457
Izmar Verhage <izmaelverhage@gmail.com>
2024-12-17 22:25:51 +0100

    Add license headers to source files

--- src/main/gui/StandaloneAppWindow.hpp
+++ src/main/gui/StandaloneAppWindow.hpp
@@ -1,3 +1,22 @@
+/*
+    This file is part of vmpc-juce, a JUCE implementation of VMPC2000XL.
+
+    vmpc-juce is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License (GPL) as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    vmpc-juce is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with vmpc-juce. If not, see <https://www.gnu.org/licenses/>.
+
+    This project uses JUCE, which is licensed under the GNU Affero General Public License (AGPL).
+    See <https://juce.com> for details.
+*/
 #pragma once
 
 #include "juce_audio_plugin_client/juce_audio_plugin_client.h"
@@ -116,7 +135,6 @@ public:
     }
 
     //==============================================================================
-    void valueChanged (Value&) override            { /* not needed */ }
 
     //==============================================================================
     File getLastFile() const
@@ -312,19 +330,6 @@ public:
     std::unique_ptr<FileChooser> stateFileChooser;
 
 private:
-    /*  This class can be used to ensure that audio callbacks use buffers with a
-        predictable maximum size.
-
-        On some platforms (such as iOS 10), the expected buffer size reported in
-        audioDeviceAboutToStart may be smaller than the blocks passed to
-        audioDeviceIOCallbackWithContext. This can lead to out-of-bounds reads if the render
-        callback depends on additional buffers which were initialised using the
-        smaller size.
-
-        As a workaround, this class will ensure that the render callback will
-        only ever be called with a block with a length less than or equal to the
-        expected block size.
-    */
     class CallbackMaxSizeEnforcer  : public AudioIODeviceCallback
     {
     public:
@@ -717,7 +722,6 @@ private:
         }
 
     private:
-        void valueChanged (Value&) override     { /* not needed */ }
 
         //==============================================================================
         void componentMovedOrResized (Component&, bool, bool) override
@@ -750,18 +754,6 @@ private:
         JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainContentComponent)
     };
 
-    /*  This custom constrainer checks with the AudioProcessorEditor (which might itself be
-        constrained) to ensure that any size we choose for the standalone window will be suitable
-        for the editor too.
-
-        Without this constrainer, attempting to resize the standalone window may set bounds on the
-        peer that are unsupported by the inner editor. In this scenario, the peer will be set to a
-        'bad' size, then the inner editor will be resized. The editor will check the new bounds with
-        its own constrainer, and may set itself to a more suitable size. After that, the resizable
-        window will see that its content component has changed size, and set the bounds of the peer
-        accordingly. The end result is that the peer is resized twice in a row to different sizes,
-        which can appear glitchy/flickery to the user.
-    */
     struct DecoratorConstrainer : public ComponentBoundsConstrainer
     {
         void checkBounds (juce::Rectangle<int>& bounds,
