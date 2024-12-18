75e8ffd3e57f18720b6eb2218f880b99ec0a0655
Izmar Verhage <izmaelverhage@gmail.com>
2024-12-18 17:38:33 +0100

    Revert "Add license headers to source files"
    This reverts commit 0d330a1c188ebc1cc8cd8f8820a9092144685457.

--- src/main/VmpcBitmapEditor.cpp
+++ src/main/VmpcBitmapEditor.cpp
@@ -1,22 +1,3 @@
-/*
-    This file is part of vmpc-juce, a JUCE implementation of VMPC2000XL.
-
-    vmpc-juce is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License (GPL) as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    vmpc-juce is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with vmpc-juce. If not, see <https://www.gnu.org/licenses/>.
-
-    This project uses JUCE, which is licensed under the GNU Affero General Public License (AGPL).
-    See <https://juce.com> for details.
-*/
 #include "VmpcBitmapEditor.hpp"
 
 #include "VmpcJuceResourceUtil.hpp"
