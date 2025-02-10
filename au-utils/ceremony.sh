2c262fb2729140e0428c36d2bb8966dfd7a74c4e
Izmar Verhage <izmaelverhage@gmail.com>
2025-02-10 13:21:16 +0100

    Add AUv2 util scripts and fix aumu v2 bus layout
    aumf still needs to be fixed!

--- au-utils/ceremony.sh
+++ au-utils/ceremony.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
-./refresh-au.sh
-./bump-version.sh
 
+if [[ "$1" != "aumu" && "$1" != "aumf" ]]; then
+    echo "Usage: $0 <AU_TYPE>"
+    echo "Available types: aumu, aumf"
+    exit 1
+fi
+
+AU_TYPE="$1"
+AU_SUBTYPE="2kXa"
+[[ "$AU_TYPE" == "aumf" ]] && AU_SUBTYPE="2kXL"
+
+AU_MANUFACTURER="Izmr"
 XCODE_PROJECT="../build-xcode/vmpc2000xl.xcodeproj"
 XCODE_TARGET="vmpc2000xl_AU"
 
-AU_TYPE="aumu"
-AU_SUBTYPE="2kXa"
-AU_MANUFACTURER="Izmr"
+./refresh-au.sh
+./bump-version.sh
 
 sleep 1
 
-xcodebuild \
-    -project $XCODE_PROJECT \
-    -scheme $XCODE_TARGET \
-    -destination 'platform=OS X,arch=arm64'
+xcodebuild -project $XCODE_PROJECT -scheme $XCODE_TARGET -destination 'platform=OS X,arch=arm64'
 
 sleep 1
 
@@ -26,10 +31,7 @@ sleep 1
 
 sleep 1
 
-xcodebuild \
-    -project $XCODE_PROJECT \
-    -scheme $XCODE_TARGET \
-    -destination 'platform=OS X,arch=arm64'
+xcodebuild -project $XCODE_PROJECT -scheme $XCODE_TARGET -destination 'platform=OS X,arch=arm64'
 
 sleep 1
 
