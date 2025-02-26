a5c649fa5ae5580be50f44316514eaa77af5177e
Izmar Verhage <izmaelverhage@gmail.com>
2025-02-26 16:42:19 +0100

    Try to fix Windows build again

---
+++ src/main/gui/vector/TooltipOverlay.cpp
@@ -0,0 +1,37 @@
+#include "TooltipOverlay.hpp"
+
+#include "KeyTooltip.hpp"
+
+using namespace vmpc_juce::gui::vector;
+
+TooltipOverlay::TooltipOverlay()
+{
+    setWantsKeyboardFocus(false);
+    setInterceptsMouseClicks(false, false);
+}
+
+void TooltipOverlay::setAllKeyTooltipsVisibility(const bool visibleEnabled)
+{
+    for (auto &c : getChildren())
+    {
+        if (dynamic_cast<KeyTooltip*>(c) == nullptr)
+        {
+            continue;
+        }
+        c->setVisible(visibleEnabled);
+    }
+}
+
+void TooltipOverlay::setKeyTooltipVisibility(const std::string label, const bool visibleEnabled)
+{
+    for (auto &c : getChildren())
+    {
+        if (auto t = dynamic_cast<KeyTooltip*>(c); t != nullptr)
+        {
+            if (t->getHardwareLabel() == label)
+            {
+                t->setVisible(visibleEnabled);
+            }
+        }
+    }
+}
--- src/main/gui/vector/TooltipOverlay.hpp
+++ src/main/gui/vector/TooltipOverlay.hpp
@@ -2,43 +2,15 @@
 
 #include <juce_gui_basics/juce_gui_basics.h>
 
-#include "KeyTooltip.hpp"
-
 namespace vmpc_juce::gui::vector {
 
     class TooltipOverlay : public juce::Component {
         public:
-            TooltipOverlay()
-            {
-                setWantsKeyboardFocus(false);
-                setInterceptsMouseClicks(false, false);
-            }
+            TooltipOverlay();
 
-            void setAllKeyTooltipsVisibility(const bool visibleEnabled)
-            {
-                for (auto &c : getChildren())
-                {
-                    if (dynamic_cast<KeyTooltip*>(c) == nullptr)
-                    {
-                        continue;
-                    }
-                    c->setVisible(visibleEnabled);
-                }
-            }
+            void setAllKeyTooltipsVisibility(const bool visibleEnabled);
 
-            void setKeyTooltipVisibility(const std::string label, const bool visibleEnabled)
-            {
-                for (auto &c : getChildren())
-                {
-                    if (auto t = dynamic_cast<KeyTooltip*>(c); t != nullptr)
-                    {
-                        if (t->getHardwareLabel() == label)
-                        {
-                            t->setVisible(visibleEnabled);
-                        }
-                    }
-                }
-            }
+            void setKeyTooltipVisibility(const std::string label, const bool visibleEnabled);
     };
 
 } // namespace vmpc_juce::gui::vector
