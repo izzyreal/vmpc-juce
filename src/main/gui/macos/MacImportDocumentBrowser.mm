987ab97d5618aa0e30eb7760a64aa63cf1249741
Izmar Verhage <izmaelverhage@gmail.com>
2025-03-01 19:10:52 -0500

    Make ChatGPT stuff build

--- src/main/gui/macos/MacImportDocumentBrowser.mm
+++ src/main/gui/macos/MacImportDocumentBrowser.mm
@@ -1,1 +1,185 @@
-ImportDocumentBrowser.mm
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if TARGET_OS_MAC && !TARGET_OS_IPHONE
+
+#include "ImportDocumentUrlProcessor.hpp"
+
+#include <AppKit/AppKit.h>
+
+@interface MyDelegate : NSObject <NSOpenSavePanelDelegate>
+
+@property (assign) vmpc_juce::gui::macos::ImportDocumentUrlProcessor* urlProcessor;
+@property (assign) NSProgressIndicator* progressIndicator;
+@property (assign) NSWindow* window;
+@property (assign) BOOL overwriteNone;
+@property (assign) BOOL overwriteAll;
+
+@end
+
+@implementation MyDelegate
+
+- (void)showCopyProgressIndicator {
+  dispatch_sync(dispatch_get_main_queue(), ^{
+    if (!_progressIndicator) {
+      [self setProgressIndicator:[[NSProgressIndicator alloc] init]];
+      [_progressIndicator setStyle:NSProgressIndicatorBarStyle];
+      [_progressIndicator setIndeterminate:NO];
+      [_progressIndicator setMinValue:0.0];
+      [_progressIndicator setMaxValue:1.0];
+      NSRect frame = [[_window contentView] frame];
+      [_progressIndicator setFrame:NSMakeRect(20, frame.size.height - 40, frame.size.width - 40, 20)];
+      [[_window contentView] addSubview:_progressIndicator];
+    }
+  });
+}
+
+- (void)updateProgressIndicator:(float)progress {
+  dispatch_async(dispatch_get_main_queue(), ^{
+    [_progressIndicator setDoubleValue:progress];
+  });
+}
+
+- (void)handleDir:(NSURL*)url relativeDir:(NSString*)relativeDir {
+  NSFileManager *fileManager = [NSFileManager defaultManager];
+  NSArray<NSURL*>* urls = [fileManager contentsOfDirectoryAtURL:url includingPropertiesForKeys:@[NSURLNameKey, NSURLIsDirectoryKey, NSURLContentModificationDateKey] options:NSDirectoryEnumerationSkipsHiddenFiles error:nil];
+  
+  if (urls) {
+    [self handleURLs:urls relativeDir:relativeDir];
+  }
+}
+
+- (BOOL)showOverwriteAlert:(NSString*)relativeFileName {
+  NSString* msg = [NSString stringWithFormat:@"File %@ exists. Overwrite?", relativeFileName];
+  NSAlert *alert = [[NSAlert alloc] init];
+  [alert setMessageText:@"File exists"];
+  [alert setInformativeText:msg];
+  [alert addButtonWithTitle:@"Yes"];
+  [alert addButtonWithTitle:@"No"];
+  [alert addButtonWithTitle:@"All"];
+  [alert addButtonWithTitle:@"None"];
+  NSModalResponse response = [alert runModal];
+
+    if (response == NSAlertThirdButtonReturn) {
+        _overwriteAll = YES;
+        return YES;  // "All"
+    } else if (response == NSAlertSecondButtonReturn) {
+        _overwriteAll = NO;
+        return NO;   // "No"
+    } else if (response == NSAlertFirstButtonReturn) {
+        return YES;  // "Yes"
+    } else {
+        return NO;   // "None" (any other response)
+    }
+}
+
+- (void)handleFile:(NSURL*)url relativeDir:(NSString*)relativeDir {
+  NSArray<NSString*>* allowedExtensions = @[@"wav",@"snd",@"aps",@"pgm",@"all",@"mid"];
+  
+  if (![allowedExtensions containsObject:url.pathExtension.lowercaseString]) {
+    return;
+  }
+  
+  NSString* filename = [url lastPathComponent];
+
+  if (_urlProcessor->destinationExists(filename.UTF8String, relativeDir.UTF8String)) {
+    NSString* relativeFileName = [NSString stringWithFormat:@"%@%@", relativeDir, filename];
+    
+    if (_overwriteNone) {
+      return;
+    }
+    
+    if (!_overwriteAll && ![self showOverwriteAlert:relativeFileName]) {
+      return;
+    }
+  }
+  
+  __block NSData *data = nil;
+  NSFileCoordinator *coordinator = [[NSFileCoordinator alloc] initWithFilePresenter:nil];
+  [coordinator coordinateReadingItemAtURL:url options:0 error:nil byAccessor:^(NSURL *newURL) {
+    data = [NSData dataWithContentsOfURL:newURL];
+  }];
+  
+  const long BUFFER_LEN = 1024;
+  
+  uint8_t buffer[BUFFER_LEN];
+  
+  [self showCopyProgressIndicator];
+  
+  unsigned long bytesAvailable = static_cast<unsigned long>(data.length);
+  unsigned long readPos = 0;
+  auto oStream = _urlProcessor->openOutputStream(filename.UTF8String, relativeDir.UTF8String);
+  
+  while (bytesAvailable > 0) {
+    unsigned long bytesToWrite = std::min(static_cast<long>(bytesAvailable), BUFFER_LEN);
+    
+    [data getBytes:&buffer range:NSMakeRange(readPos, bytesToWrite)];
+    oStream->write(reinterpret_cast<char*>(buffer), static_cast<std::streamsize>(bytesToWrite));
+    
+    float progress = readPos / (float) data.length;
+    
+    [self updateProgressIndicator:progress];
+    
+    bytesAvailable -= bytesToWrite;
+    readPos += bytesToWrite;
+  }
+}
+
+- (void)handleURLs:(NSArray<NSURL*>*)urls relativeDir:(NSString*)relativeDir {
+  for (NSURL* url in urls) {
+    @autoreleasepool {
+      NSNumber *isDirectory;
+      BOOL success = [url getResourceValue:&isDirectory forKey:NSURLIsDirectoryKey error:nil];
+
+      if (success && [isDirectory boolValue]) {
+        NSString* dirName = [url lastPathComponent];
+        NSString* newRelativeDir = [NSString stringWithFormat:@"%@%@/", relativeDir, dirName];
+        [self handleDir:url relativeDir:newRelativeDir];
+      } else {
+        [self handleFile:url relativeDir:relativeDir];
+      }
+    }
+  }
+}
+
+- (void)openDocumentPicker:(NSWindow*)window {
+  NSOpenPanel *panel = [NSOpenPanel openPanel];
+  [panel setAllowsMultipleSelection:YES];
+  [panel setCanChooseFiles:YES];
+  [panel setCanChooseDirectories:YES];
+  [panel setDelegate:self];
+  
+  if ([panel runModal] == NSModalResponseOK) {
+    NSArray<NSURL*>* urls = [panel URLs];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_BACKGROUND, 0), ^{
+      [self handleURLs:urls relativeDir:@""];
+      dispatch_async(dispatch_get_main_queue(), ^{
+        _urlProcessor->initFiles();
+      });
+    });
+  }
+}
+
+@end
+
+@implementation NSWindow (DocumentPicker)
+
+-(void) openMacDocumentPicker:(vmpc_juce::gui::macos::ImportDocumentUrlProcessor*)urlProcessor {
+  MyDelegate* delegate = [[MyDelegate alloc] init];
+  [delegate setOverwriteAll:NO];
+  [delegate setOverwriteNone:NO];
+  [delegate setUrlProcessor:urlProcessor];
+  [delegate setWindow:self];
+  [delegate openDocumentPicker:self];
+}
+
+@end
+
+void doOpenMacImportDocumentPicker(vmpc_juce::gui::macos::ImportDocumentUrlProcessor* urlProcessor, void* nativeWindowHandle) {
+  NSView* view = (__bridge NSView*)nativeWindowHandle;
+  NSWindow* window = [view window];
+  [window openMacDocumentPicker:urlProcessor];
+}
+
+#endif
+#endif
+
--- src/main/gui/macos/MacOSNSWindowWithExportActivityView.mm
+++ src/main/gui/macos/MacOSNSWindowWithExportActivityView.mm
@@ -1,1 +1,128 @@
-IosUIWindowWithExportActivityView.mm
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#if TARGET_OS_OSX
+
+#include <vector>
+#include <AppKit/AppKit.h>
+
+#include "Mpc.hpp"
+#include "sampler/Sampler.hpp"
+#include "file/aps/ApsParser.hpp"
+#include "file/all/AllParser.hpp"
+#include "file/sndwriter/SndWriter.hpp"
+#include "lcdgui/screens/LoadScreen.hpp"
+#include "disk/AbstractDisk.hpp"
+#include "disk/MpcFile.hpp"
+
+#include "miniz.h"
+
+@implementation NSWindow (WithExportActivityView)
+
+- (NSMutableArray<NSString *> *)writeApsAllAndSnd:(mpc::Mpc *)mpc {
+    
+    NSString *tempDirectory = NSTemporaryDirectory();
+    NSMutableArray<NSString *> *filePathsArray = [NSMutableArray array];
+
+    const std::vector<char>& apsData = mpc::file::aps::ApsParser(*mpc, "ALL_PGMS").saveBytes;
+    NSData *data = [NSData dataWithBytes:apsData.data() length:apsData.size()];
+    NSString *apsDataFilePath = [tempDirectory stringByAppendingPathComponent:@"ALL_PGMS.APS"];
+    if ([data writeToFile:apsDataFilePath atomically:YES]) { [filePathsArray addObject:apsDataFilePath]; }
+
+    const std::vector<char>& allData = mpc::file::all::AllParser(*mpc).saveBytes;
+    data = [NSData dataWithBytes:allData.data() length:allData.size()];
+    NSString *allDataFilePath = [tempDirectory stringByAppendingPathComponent:@"ALL_SEQS.ALL"];
+    if ([data writeToFile:allDataFilePath atomically:YES]) { [filePathsArray addObject:allDataFilePath]; }
+
+    for (auto& sound : mpc->getSampler()->getSounds()) {
+        const std::vector<char>& sndData = mpc::file::sndwriter::SndWriter(sound.get()).getSndFileArray();
+        data = [NSData dataWithBytes:sndData.data() length:sndData.size()];
+        NSString *soundName = [NSString stringWithUTF8String:sound->getName().c_str()];
+        NSString *sndDataFilePath = [tempDirectory stringByAppendingPathComponent:[soundName stringByAppendingString:@".SND"]];
+        if ([data writeToFile:sndDataFilePath atomically:YES]) { [filePathsArray addObject:sndDataFilePath]; }
+    }
+    
+    return filePathsArray;
+}
+
+- (void)createZip:(fs::path)sourcePath filePathsArray:(NSMutableArray<NSString *> *)filePathsArray {
+    NSString *tempDirectory = NSTemporaryDirectory();
+    NSString *zipFileName = [[NSString stringWithUTF8String:sourcePath.filename().string().c_str()] stringByAppendingString:@".zip"];
+    NSString *zipFilePath = [tempDirectory stringByAppendingPathComponent:zipFileName];
+
+    mz_zip_archive zipArchive;
+    memset(&zipArchive, 0, sizeof(zipArchive));
+    mz_bool status = mz_zip_writer_init_file(&zipArchive, zipFilePath.UTF8String, 0);
+
+    if (!status) {
+        NSLog(@"Failed to initialize zip archive");
+        return;
+    }
+
+    for (const auto& entry : fs::recursive_directory_iterator(sourcePath)) {
+        if (!entry.is_directory()) {
+            std::string relativePath = fs::relative(entry.path(), sourcePath).string();
+            mz_zip_writer_add_file(&zipArchive, relativePath.c_str(), entry.path().c_str(), "", 0, MZ_BEST_COMPRESSION);
+        }
+    }
+
+    mz_zip_writer_finalize_archive(&zipArchive);
+    mz_zip_writer_end(&zipArchive);
+
+    [filePathsArray addObject:zipFilePath];
+}
+
+- (NSMutableArray<NSString *> *)getSelectedFileOrDirectory:(mpc::Mpc *)mpc {
+    auto selectedFile = mpc->screens->get<mpc::lcdgui::screens::LoadScreen>("load")->getSelectedFile();
+    NSMutableArray<NSString *> *filePathsArray = [NSMutableArray array];
+
+    if (selectedFile->isDirectory()) {
+        [self createZip:selectedFile->getPath() filePathsArray:filePathsArray];
+    } else {
+        NSString *filePath = [NSString stringWithUTF8String:selectedFile->getPath().c_str()];
+        [filePathsArray addObject:filePath];
+    }
+
+    return filePathsArray;
+}
+
+- (void)showSharingOptions:(NSArray<NSString *> *)filePaths {
+    NSMutableArray *fileURLs = [NSMutableArray array];
+    for (NSString *path in filePaths) {
+        [fileURLs addObject:[NSURL fileURLWithPath:path]];
+    }
+
+    NSSharingServicePicker *picker = [[NSSharingServicePicker alloc] initWithItems:fileURLs];
+    [picker showRelativeToRect:NSMakeRect(0, 0, 200, 100) ofView:self.contentView preferredEdge:NSRectEdgeMinY];
+}
+
+- (void)presentShareOptions:(mpc::Mpc *)mpc {
+    NSAlert *alert = [[NSAlert alloc] init];
+    [alert setMessageText:@"Share Options"];
+    [alert setInformativeText:@"Choose a sharing option."];
+    [alert addButtonWithTitle:@"Share APS, SNDs, and ALL"];
+    [alert addButtonWithTitle:@"Share Selected File or Directory"];
+    [alert addButtonWithTitle:@"Cancel"];
+
+    NSInteger button = [alert runModal];
+
+    NSMutableArray<NSString *> *filePathsArray = [NSMutableArray array];
+
+    if (button == NSAlertFirstButtonReturn) {
+        [filePathsArray addObjectsFromArray:[self writeApsAllAndSnd:mpc]];
+        [self showSharingOptions:filePathsArray];
+    } else if (button == NSAlertSecondButtonReturn) {
+        [filePathsArray addObjectsFromArray:[self getSelectedFileOrDirectory:mpc]];
+        [self showSharingOptions:filePathsArray];
+    }
+}
+
+@end
+
+void doPresentShareOptions(void* nativeWindowHandle, mpc::Mpc* mpc) {
+    auto nswindow = (NSWindow*) nativeWindowHandle;
+    [nswindow presentShareOptions:mpc];
+}
+
+#endif
+#endif
+
