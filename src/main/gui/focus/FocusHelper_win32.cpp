b4dd0a6dd410f6ff00509c3900659ed842931e4c
Izmar Verhage <izmaelverhage@gmail.com>
2025-12-20 13:07:31 +0100

    Fix focus helper (at least for renoise)

--- src/main/gui/focus/FocusHelper_win32.cpp
+++ src/main/gui/focus/FocusHelper_win32.cpp
@@ -9,7 +9,14 @@ extern "C" bool isEditorKeyWindow(void *componentPeerNativeHandle)
         return false;
     }
 
-    HWND fg = GetForegroundWindow();
-    return fg && fg == hwnd;
+    // This also works (only tested with Renoise), but I'm not sure which one
+    // is better. GetFocus() seems to be the 'correct' one, because we're
+    // looking for keyboard focus. But we may need to dig into the details after
+    // trying against a few different hosts.
+    //
+    // HWND fg = GetForegroundWindow();
+
+    HWND fg = GetFocus();
+    return fg && (fg == hwnd || IsChild(fg, hwnd));
 }
 #endif
